8978f1fa2475947e17e25987c8d571a7ea9c762d
Larry Warner <larry.warner@gmail.com>
2014-05-20 14:18:57 -0700

    Complete and working
    Signed-off-by: Brian Cunnie <cunnie@pivotallabs.com>

--- finduuid.pl
+++ finduuid.pl
@@ -6,9 +6,16 @@ use Digest::MD5 qw(md5);
 my $uuidregex="[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}";
 
 sub color_ids {
+    # @bad_combos = ( "$background$foreground", ... )
+    my @bad_combos = ( "4030", "4090", "4131", "4135", "4191", "4232", "4233",
+	"4294", "4239", "4332", "4333", "4339", "4434", "4531", "4535", "4591",
+	"4636", "4737", "4797", "10030", "10090", "10131", "10135", "10191",
+	"10292", "10393", "10395", "10432", "10494", "10439", "10593", "10595",
+	"10696", "10737", "10797", "4930", "4990" );
     my @color_strings = ();
-    for my $foreground ( 31 .. 37, 90..97, 39 ) {
+    for my $foreground ( 30 .. 37, 90..97, 39 ) {
 	for my $background ( 40 .. 47, 100 .. 107, 49 ) {
+	    next if grep(/$background$foreground/, @bad_combos);
 	    push(@color_strings, "[0;$background;${foreground}m");
 	}
     }
@@ -44,15 +51,13 @@ sub uuid_substring {
 sub main  {
     while (<>) {
 	if (/(^.*)($uuidregex)(.*$)/)  {
-	    #my ($start, $uuidstring, $end) = ($1, $2, $3);
 	    my @count = $_ =~ m/$uuidregex/g;
 	    my $substring = $_;
 	    for my $index (@count) {
 		(my $formatted, $substring) = &uuid_substring($substring);
 		print $formatted
 	    }
-	    print scalar @count; print "\n";
-	    #print $start, &colorize($uuidstring), $end, "\n";
+	    print $substring."\n";
 	} else {
 	    print;
 	}
